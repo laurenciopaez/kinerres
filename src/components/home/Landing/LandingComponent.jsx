7c8a50b32d4346468871735bf0710c3ca9765cda
laurenciopaez <laurencio314gmail.com>
2024-01-06 14:39:33 -0300

    avances

--- src/components/home/Landing/LandingComponent.jsx
+++ src/components/home/Landing/LandingComponent.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Footer from "@/components/subcomps/Footer";
 import NavigationBar from "@/components/subcomps/NavigationBar";
+import ContactForm from "./subcomps/ContactForm";
 
 const LandingComponent = () => {
   return (
-    <div
-      className="bg-gradient-to-r from-serif-mint-light via-serif-mint-brakpoint to-serif-mint-dark"
-    >
+    <div className="bg-gradient-to-r from-serif-mint-light via-serif-mint-brakpoint to-serif-mint-dark">
       <NavigationBar />
       <div className="text-center flex w-full flex-col h-[65vh]">
-        <h1 className="font-semibold text-7xl mb-4 mt-28 text-blue-600">
+        <h1 className="font-semibold text-7xl mb-4 mt-28 text-blue-600 ">
           Programa Kinerres
         </h1>
         <h2 className="font-medium text-2xl mb-2 text-black">
@@ -22,7 +21,10 @@ const LandingComponent = () => {
 
       <div className="mt-20 flex flex-row text-black w-[90%] mx-auto h-[40vh]">
         <div className="flex flex-row w-1/2">
-          <img src="/images/lic2.jpg" className="w-[45%] h-fit max-h-[45vh] pl-6"  />
+          <img
+            src="/images/lic2.jpg"
+            className="w-[45%] h-fit max-h-[45vh] ml-6 border-2 border-white"
+          />
           <div className="flex flex-col w-1/2">
             <h2 className="font-medium text-3xl ml-3">Lic. Deborah Gelabert</h2>
             <h3 className="font-normal text-lg ml-3">
@@ -32,7 +34,10 @@ const LandingComponent = () => {
           </div>
         </div>
         <div className="flex flex-row w-1/2">
-          <img src="/images/lic1.jpg" className="w-[45%] h-fit max-h-[45vh] pl-6" />
+          <img
+            src="/images/lic1.jpg"
+            className="w-[45%] h-fit max-h-[45vh] ml-6 border-2 border-white"
+          />
           <div className="flex flex-col w-1/2">
             <h2 className="font-medium text-3xl ml-3">Lic. Eugenia Mancini</h2>
             <h3 className="font-normal text-lg ml-3">
@@ -42,29 +47,36 @@ const LandingComponent = () => {
         </div>
       </div>
 
-      <div className="mt-20 flex flex-row text-black mb-8 pl-6 h-[40vh] items-center">
-        <h1 className="font-semibold text-5xl w-1/2">Quienes somos?</h1>
-        <p className="w-1/2 text-normal pr-10">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-          voluptatum dolorum cumque eveniet voluptate sint quod sequi distinctio
-          provident, perferendis eum ipsa reiciendis sunt totam velit id aliquam
-          at officiis.
+      <div className="mt-20 flex flex-row text-black mb-8 h-[40vh] items-center">
+        <h1 className="font-semibold text-5xl w-1/2 content-center justify-center flex">
+          Quienes somos?
+        </h1>
+        <p className="w-1/2 text-lg justify-center flex text-center pr-20">
+          KineRRes ya es una marca registrada en Rehabilitación Respiratoria, es
+          un programa dedicado al entrenamiento de pacientes con diversas
+          patologías respiratorias con o sin requerimiento de de Oxigenoterapia,
+          cuya indicación está dada por médicos neumonólogos, clínicos,
+          oncólogos y cardiólogos.
         </p>
       </div>
 
       <div className="flex flex-row text-black p-4 h-[60vh]">
         <div className="flex flex-col my-auto">
-          <h2 className="font-medium text-2xl">Titulo de que hacen</h2>
+          <h2 className="font-medium text-2xl">
+            ¿Qué es la Rehabilitación Respiratoria?
+          </h2>
           <p className="font-normal text-lg m-4">
             {" "}
-            clasLorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Asperiores dolor placeat iure ipsa, provident unde eveniet possimus
-            dolore dolores, quam hic perspiciatis. Ab impedit, suscipit
-            reprehenderit consectetur quia assumenda possimus?
+            Es un programa multidisciplinario para la atención o cuidado del
+            paciente con afección respiratoria crónica.
+            <br />
+            Se ajusta individualmente para lograr al máximo su actividad física,
+            social y mayor autonomía e independencia funcional, a través de una
+            actividad física supervisada.
           </p>
         </div>
         <div className="flex flex-col my-auto">
-          <h2 className="font-medium text-2xl">Titulo de que hacen</h2>
+          <h2 className="font-medium text-2xl">¿A quienes está dirigido?</h2>
           <p className="font-normal text-lg m-4">
             {" "}
             clasLorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -110,13 +122,13 @@ const LandingComponent = () => {
 
           <div className="w-1/3">
             <h1 className="font-semibold text-lg mb-2">
-              Titulo de lo que hacen
+              Beneficios de la Rehabilitación Respiratoria
             </h1>
             <p className="font-normal text-base">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Quibusdam quia eum nihil ab dolor labore id aspernatur tempore
-              atque, nemo perspiciatis repudiandae vitae culpa architecto maxime
-              consectetur. Molestias, nihil libero?
+              Actualmente, hay múltiple evidencia científica que la
+              Rehabilitación pulmonar mejora notablemente la tolerancia al
+              ejercicio, disminuye la sensación de falta de aire y mejora la
+              calidad de vida de los pacientes.
             </p>
           </div>
         </div>
@@ -126,29 +138,47 @@ const LandingComponent = () => {
         <h1 className="w-ful text-center font-semibold text-5xl">
           Miranos en nuestro youtube
         </h1>
-        <div className="flex flex-row">
-          <iframe src="" frameborder="0"></iframe>
-          <iframe src="" frameborder="0"></iframe>
-          <iframe src="" frameborder="0"></iframe>
+        <div className="flex flex-row p-12 h-[50vh]">
+          <div className="w-1/2 p-2 content-center flex justify-center">
+            <iframe
+              src="https://www.youtube.com/embed/BLvn52j75tk"
+              title='PROGRAMA DE REHABILITACIÓN PULMONAR "KINERRES" DE LA UNIDAD DE CARDIOLOGÍA CLÍNICA'
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              className="w-4/5 h-full"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="w-1/2 p-2 content-center flex justify-center">
+            <iframe
+              src="https://www.youtube.com/embed/4rr4OiIOYGU"
+              title="PRIMERA JORNADA DE CAPACITACIÓN SOBRE ENFERMEDAD PULMONAR INTERSTICIAL"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              className="w-4/5 h-full"
+              allowfullscreen
+            ></iframe>
+          </div>
         </div>
       </div>
 
-
       <div className="bg-cyan-950 mt-20 flex flex-col p-10">
-          <div>
-            <h1 className="text-5xl font-semibold text-white">Contactar</h1>
-            <h2 className="w-1/2 mt-6 text-lg">No dudes en ponerte en contacto con nosotros mediante la siguiente informacion de contacto o utiliza el formulario para enviarnos un mensaje.</h2>
+        <div>
+          <h1 className="text-5xl font-semibold text-white">Contactar</h1>
+          <h2 className="w-1/2 mt-6 text-lg">
+            No dudes en ponerte en contacto con nosotros mediante la siguiente
+            informacion de contacto o utiliza el formulario para enviarnos un
+            mensaje.
+          </h2>
+        </div>
+        <div className="flex flex-row">
+          <div className="flex flex-col w-1/2">
+            <ContactForm />
           </div>
-          <div className="flex flex-row">
-              <div className="flex flex-col w-1/2">
-              <form>
-                {/* formulario de contacto */}
-              </form>
-              </div>
-              <div className="w-1/2 p-3">
-                <img src="/images/mapa.png"/>
-              </div>
+          <div className="w-1/2 p-3">
+            <img src="/images/mapa.png" />
           </div>
+        </div>
       </div>
 
       <Footer />

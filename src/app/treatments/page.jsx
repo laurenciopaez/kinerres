816d07981f69d5cd0963a62fb5a84c841ddee48f
laurenciopaez <laurencio314gmail.com>
2024-01-06 15:21:44 -0300

    s

--- src/app/treatments/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import React from "react";
-import TreatmentsComp from "@/components/treatments/treatmentsComp";
-export default function Treatments() {
-  return <TreatmentsComp />;
-}

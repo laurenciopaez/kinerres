f6d7f73046141d37c7b3771f121fa3385c338cb2
laurenciopaez <laurencio314gmail.com>
2024-01-14 14:18:32 -0300

    images render

--- src/components/home/Landing/subcomps/PicturesSpace.jsx
+++ src/components/home/Landing/subcomps/PicturesSpace.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Image from 'next/image';
 
 const PicturesSpace = () => {
   const picsArray = [
@@ -9,58 +10,76 @@ const PicturesSpace = () => {
   ];
 
   const [importartPic, setImportartPic] = useState(picsArray[0]);
+  const [verticalImages, setVerticalImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImageInfo = async () => {
+      const verticalImageList = await Promise.all(
+        picsArray.map((el) => {
+          return new Promise((resolve) => {
+            const tempImage = new Image();
+            tempImage.src = el;
+            tempImage.onload = () => {
+              if (tempImage.height > tempImage.width) {
+                resolve(
+                  <div
+                    key={el}
+                    className="relative mb-2 bg-gradient-to-b from-white via-blue-100 to-green-700 shadow-md max-h-[25vh] overflow-y-hidden"
+                  >
+                    <Image
+                      src={el}
+                      alt="Espacio de trabajo"
+                      className="mx-auto shadow-md rounded-sm flex my-auto w-full hover:scale-90 duration-300 max-w-[80%] top-1/2 transform -translate-y-1/2"
+                      onClick={() => setImportartPic(el)}
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                );
+              } else {
+                resolve(
+                  <div
+                    key={el}
+                    className="mb-2 bg-gradient-to-b from-white via-blue-100 to-green-700 shadow-md max-h-[25vh] overflow-y-hidden"
+                  >
+                    <Image
+                      src={el}
+                      alt="Espacio de trabajo"
+                      className="mx-auto shadow-md rounded-sm flex my-auto w-full hover:scale-90 duration-300 max-w-[80%]"
+                      onClick={() => setImportartPic(el)}
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                );
+              }
+            };
+          });
+        })
+      );
+
+      setVerticalImages(verticalImageList.filter(Boolean));
+    };
+
+    fetchImageInfo();
+  }, [picsArray]);
+
   return (
-    <div className="flex flex-col lg:flex-row xl:flex-row text-black  bg-opacity-25 bg-white pt-10 pb-10 border-b-2 border-Verde mx-auto">
-      {/* aca va la imagen principal */}
+    <div className="flex flex-col lg:flex-row xl:flex-row text-black bg-opacity-25 bg-white pt-10 pb-10 border-b-2 border-Verde mx-auto">
+      {/* Main image */}
       <div className="flex mx-auto min-w-[50%] lg:max-w-[66%] xl:max-w-[66%] mb-2">
-        <img
+        <Image
           src={importartPic}
+          alt="Espacio de trabajo"
           className="overflow-y-hidden h-full mx-auto lg:shadow-md xl:shadow-md rounded-sm border flex my-auto max-h-[80vh] max-w-[80%]"
+          layout="fill"
+          objectFit="cover"
         />
       </div>
 
-      {/* aca van las imagenes que se podes clickear */}
+      {/* Clickable images */}
       <div className="lg:w-1/3 xl:w-1/3 flex-col flex lg:pr-20 xl:pr-20 w-full lg:pl-0 xl:pl-0 my-auto mx-auto">
-        {picsArray.map((el, index) => {
-          const tempImage = new Image();
-          tempImage.src = el;
-          if (tempImage.height > tempImage.width) {
-            if (el !== importartPic) {
-              return (
-                <div
-                  key={index}
-                  className="relative mb-2 lg:bg-gradient-to-b xl:bg-gradient-to-b from-white via-blue-100  to-green-700 lg:shadow-md xl:shadow-md max-h-[25vh] overflow-y-hidden"
-                >
-                  <img
-                    key={index}
-                    src={el}
-                    alt="Espacio de trabajo"
-                    className=" mx-auto lg:shadow-md xl:shadow-md rounded-sm flex my-auto w-full mt-2  hover:scale-90 duration-300 max-w-[80%]  top-1/2 transform  -translate-y-1/2"
-                    onClick={() => setImportartPic(el)}
-                  />
-                </div>
-              );
-            }
-          } else {
-            if (el !== importartPic) {
-              return (
-                <div
-                  key={index}
-                  className=" mb-2 g:bg-gradient-to-b xl:bg-gradient-to-b from-white via-blue-100 mt-2  to-green-700 lg:shadow-lg xl:shadow-lg max-h-[25vh] overflow-y-hidden"
-                >
-                  <img
-                    key={index}
-                    src={el}
-                    alt="Espacio de trabajo"
-                    className=" mx-auto shadow-md rounded-sm flex my-auto w-full  hover:scale-90 duration-300 max-w-[80%]"
-                    onClick={() => setImportartPic(el)}
-                  />
-                </div>
-              );
-            }
-            return null;
-          }
-        })}
+        {verticalImages}
       </div>
     </div>
   );

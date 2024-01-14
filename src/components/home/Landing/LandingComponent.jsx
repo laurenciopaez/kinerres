6ee56d1b7c028de1657a743bacfe6bae34abc68f
laurenciopaez <laurencio314gmail.com>
2024-01-14 13:03:05 -0300

    Pics

--- src/components/home/Landing/LandingComponent.jsx
+++ src/components/home/Landing/LandingComponent.jsx
@@ -6,8 +6,16 @@ import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 const LandingComponent = () => {
+  const picsArray = [
+    "/images/espacio1.jpeg",
+    "/images/espacio2.jpeg",
+    "/images/espacio3.jpeg",
+    "/images/espacio4.jpeg",
+  ];
   const [isHovered, setIsHovered] = useState(false);
   const [isHovered2, setIsHovered2] = useState(false);
+
+  const [importartPic, setImportartPic] = useState(picsArray[0]);
 
   const [animation1, setAnimation1] = useState(false);
   const [animation2, setAnimation2] = useState(false);
@@ -48,9 +56,8 @@ const LandingComponent = () => {
         <img
           src="/images/wp.png"
           alt="WhatsApp"
-          width='100px'
+          width="100px"
           className="fixed bottom-10 right-10 z-50 hover:scale-105 duration-75 transition"
-          
         />
       </a>
       <NavigationBar scrollToSection={scrollToSection} />
@@ -121,7 +128,7 @@ const LandingComponent = () => {
         className="flex lg:flex-row xl:flex-row flex-col items-center text-center text-black p-4  mt-52 border-t-2 border-b-2 border-Verde"
         id="tratamientos"
       >
-        <div className="flex flex-col lg:w-1/2 xl:w-1/2 ml-10 p-2 h-auto " >
+        <div className="flex flex-col lg:w-1/2 xl:w-1/2 ml-10 p-2 h-auto ">
           <div
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
@@ -172,6 +179,7 @@ const LandingComponent = () => {
         className="flex flex-col text-black  bg-opacity-25 bg-white pt-10 pb-10 border-b-2 border-Verde"
         id="nuestroEspacio"
       >
+        <h1 className="font-semibold text-5xl mx-auto mb-5">Nuestro Espacio</h1>
         <div className="flex lg:flex-row xl:flex-row flex-col">
           <div className="w-2/3 h-full mx-auto ">
             <motion.div
@@ -227,7 +235,62 @@ const LandingComponent = () => {
         </div>
       </div>
 
-      <div className="flex flex-col text-black mt-20" id="testimonios" >
+      <div className="flex flex-row text-black  bg-opacity-25 bg-white pt-10 pb-10 border-b-2 border-Verde mx-auto">
+        {/* aca va la imagen principal */}
+        <div className="flex lg:flex-row xl:flex-row flex-col mx-auto min-w-[50%] max-w-[66%]">
+          <img
+            src={importartPic}
+            className="overflow-y-hidden h-full mx-auto shadow-md rounded-sm border flex my-auto max-h-[80vh] max-w-[80%]"
+          />
+        </div>
+
+        {/* aca van las imagenes que se podes clickear */}
+        <div className="lg:w-1/3 xl:w-1/3 flex-col flex pr-20 w-full pl-5 lg:pl-0 xl:pl-0 my-auto">
+          
+          {picsArray.map((el, index) => {
+            const tempImage = new Image();
+            tempImage.src = el;
+              if (tempImage.height > tempImage.width) {
+                if (el !== importartPic) {
+                  return (
+                    <div
+                      key={index}
+                      className="relative mb-2 bg-gradient-to-b from-white via-blue-100  to-green-700 shadow-lg max-h-[25vh] overflow-y-hidden"
+                    >
+                      <img
+                        key={index}
+                        src={el}
+                        alt="Espacio de trabajo"
+                        className=" mx-auto shadow-md rounded-sm flex my-auto w-full  hover:scale-90 duration-300 max-w-[80%]  top-1/2 transform  -translate-y-1/2"
+                        onClick={() => setImportartPic(el)}
+                      />
+                    </div>
+                  );
+                }
+              } else {
+                if (el !== importartPic) {
+                  return (
+                    <div
+                      key={index}
+                      className=" mb-2 bg-gradient-to-b from-white via-blue-100  to-green-700 shadow-lg max-h-[25vh] overflow-y-hidden"
+                    >
+                      <img
+                        key={index}
+                        src={el}
+                        alt="Espacio de trabajo"
+                        className=" mx-auto shadow-md rounded-sm flex my-auto w-full  hover:scale-90 duration-300 max-w-[80%]"
+                        onClick={() => setImportartPic(el)}
+                      />
+                    </div>
+                  );
+                }
+              return null;
+            };
+          })}
+        </div>
+      </div>
+
+      <div className="flex flex-col text-black mt-20" id="testimonios">
         <h1 className="w-ful text-center font-semibold lg:text-5xl xl:text-5xl text-3xl mb-10">
           Encontranos en{" "}
           <span className="hover:text-red-500 transition duration-150">
